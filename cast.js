58bb3554e38990c6b7f8589c4622f645b7339716
Marco Garay <mgaray@ooyala.com>
2017-08-22 16:01:27 -0500

    another fix

--- cast.js
+++ cast.js
@@ -429,6 +429,7 @@
                   // Controlled extention of displaying loading screen, currently set to 3 seconds.  
                   // ATTN: Receiver HAS TO publish CAN_PLAY in case of autoPlay
                   setTimeout(function() {
+                    player.mb.publish(OO.EVENTS.CAN_PLAY);
                     player.play();
                     //player.mb.publish(OO.EVENTS.INITIAL_PLAY);
                   }, 3000);
